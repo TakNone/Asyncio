0e2423ae584a00026e4b12f8348051eaa4bbadeb
Your New Name <mrtaknone@gmail.com>
2026-06-11 10:18:38 -0700

    Update StreamSocket.php

--- src/Socket/StreamSocket.php
+++ src/Socket/StreamSocket.php
@@ -21,8 +21,8 @@ class StreamSocket {
 				$this->socket = new Socket($domain,$type,$protocol);
 				break;
 			case 'Revolt':
-				$this->socket = socket_create($domain,$type,$protocol);
-				socket_set_nonblock($this->socket);
+				$this->socket = @socket_create($domain,$type,$protocol);
+				@socket_set_nonblock($this->socket);
 				break;
 		}
 	}
@@ -30,19 +30,19 @@ class StreamSocket {
 		$this->check($address,$port);
 		return match(Loop::name()){
 			'Swoole' => $this->socket->bind($address,$port),
-			'Revolt' => socket_bind($this->socket,$address,$port)
+			'Revolt' => @socket_bind($this->socket,$address,$port)
 		};
 	}
 	public function listen(int $backlog) : bool {
 		return match(Loop::name()){
 			'Swoole' => $this->socket->listen($backlog),
-			'Revolt' => socket_listen($this->socket,$backlog)
+			'Revolt' => @socket_listen($this->socket,$backlog)
 		};
 	}
 	public function accept(float $timeout = -1) : object | false {
 		$client = match(Loop::name()){
 			'Swoole' => $this->socket->accept($timeout),
-			'Revolt' => socket_accept($this->socket)
+			'Revolt' => @socket_accept($this->socket)
 		};
 		return is_object($client) ? new class($client) extends StreamSocket {
 			public function __construct(protected object $socket){
@@ -58,8 +58,7 @@ class StreamSocket {
 				if(@socket_connect($this->socket,$host,$port)){
 					return true;
 				} else {
-					$error = socket_last_error($this->socket);
-					if($error === SOCKET_EINPROGRESS or $error === SOCKET_EALREADY){
+					if(in_array(socket_last_error($this->socket),array(SOCKET_EINPROGRESS,SOCKET_EALREADY))){
 						$cancellation = $timeout > 0 ? new TimeoutCancellation($timeout) : null;
 						$cancellation?->throwIfCancelled();
 						$suspension = Loop::getSuspension();
@@ -84,62 +83,84 @@ class StreamSocket {
 		}
 	}
 	public function write(string $data,float $timeout = -1,int $flags = 0) : int | false {
-		switch(Loop::name()){
-			case 'Swoole':
-				return $this->socket->sendAll($data,$timeout);
-			case 'Revolt':
-				$result = socket_send($this->socket,$data,strlen($data),$flags);
-				if($result === false and in_array(socket_last_error($this->socket),array(SOCKET_EAGAIN,SOCKET_EWOULDBLOCK))){
-					$cancellation = $timeout > 0 ? new TimeoutCancellation($timeout) : null;
-					$cancellation?->throwIfCancelled();
-					$suspension = Loop::getSuspension();
-					$id = Loop::onWritable($this->getResource(),function(string $id) use($suspension,$data,$flags) : void {
-						Loop::cancel($id);
-						$result = socket_send($this->socket,$data,strlen($data),$flags);
-						$suspension->resume($result);
-					});
-					$cancel = $cancellation?->subscribe($this->pending[$id] = static function() use($suspension,$id) : void {
-						Loop::cancel($id);
-						$suspension->resume(false);
-					});
-					try {
-						$result = $suspension->suspend();
-					} finally {
-						unset($this->pending[$id]);
-						$cancellation?->unsubscribe($cancel);
+		if($this->isClosed() === false){
+			switch(Loop::name()){
+				case 'Swoole':
+					return $this->socket->sendAll($data,$timeout);
+				case 'Revolt':
+					$bytesWritten = 0;
+					$totalBytes = strlen($data);
+					while($bytesWritten < $totalBytes){
+						$chunk = substr($data,$bytesWritten);
+						$result = @socket_send($this->socket,$chunk,strlen($chunk),$flags);
+						if($result === false){
+							if(in_array(socket_last_error($this->socket),array(SOCKET_EAGAIN,SOCKET_EWOULDBLOCK))){
+								$cancellation = $timeout > 0 ? new TimeoutCancellation($timeout) : null;
+								$cancellation?->throwIfCancelled();
+								$suspension = Loop::getSuspension();
+								$id = Loop::onWritable($this->getResource(),function(string $id) use($suspension) : void {
+									Loop::cancel($id);
+									$suspension->resume(true);
+								});
+								$cancel = $cancellation?->subscribe($this->pending[$id] = static function() use($suspension,$id) : void {
+									Loop::cancel($id);
+									$suspension->resume(false);
+								});
+								try {
+									if($suspension->suspend() === false){
+										return $bytesWritten > 0 ? $bytesWritten : false;
+									}
+								} finally {
+									unset($this->pending[$id]);
+									$cancellation?->unsubscribe($cancel);
+								}
+								continue;
+							}
+							return $bytesWritten > 0 ? $bytesWritten : false;
+						} else if($result === 0){
+							return $bytesWritten > 0 ? $bytesWritten : false;
+						} else {
+							$bytesWritten += $result;
+						}
 					}
-				}
-				return $result;
+					return $bytesWritten;
+			}
+		} else {
+			return false;
 		}
 	}
 	public function read(? int $length = null,float $timeout = -1,int $flags = 0) : string | false {
-		switch(Loop::name()){
-			case 'Swoole':
-				return is_null($length) ? $this->socket->recv(1 << 16,$timeout) : $this->socket->recvAll($length,$timeout);
-			case 'Revolt':
-				$buffer = strval(null);
-				$result = socket_recv($this->socket,$buffer,is_null($length) ? (1 << 16) : $length,is_null($length) ? 0 : $flags);
-				if($result === false and in_array(socket_last_error($this->socket),array(SOCKET_EAGAIN,SOCKET_EWOULDBLOCK))){
-					$cancellation = $timeout > 0 ? new TimeoutCancellation($timeout) : null;
-					$cancellation?->throwIfCancelled();
-					$suspension = Loop::getSuspension();
-					$id = Loop::onReadable($this->getResource(),function(string $id) use($suspension,&$buffer,$length,$flags) : void {
-						Loop::cancel($id);
-						$result = socket_recv($this->socket,$buffer,is_null($length) ? (1 << 16) : $length,is_null($length) ? 0 : $flags);
-						$suspension->resume($result);
-					});
-					$cancel = $cancellation?->subscribe($this->pending[$id] = static function() use($suspension,$id) : void {
-						Loop::cancel($id);
-						$suspension->resume(false);
-					});
-					try {
-						$result = $suspension->suspend();
-					} finally {
-						unset($this->pending[$id]);
-						$cancellation?->unsubscribe($cancel);
+		if($this->isClosed() === false){
+			switch(Loop::name()){
+				case 'Swoole':
+					return is_null($length) ? $this->socket->recv(1 << 16,$timeout) : $this->socket->recvAll($length,$timeout);
+				case 'Revolt':
+					$buffer = strval(null);
+					$result = @socket_recv($this->socket,$buffer,is_null($length) ? (1 << 16) : $length,is_null($length) ? 0 : $flags);
+					if($result === false and in_array(socket_last_error($this->socket),array(SOCKET_EAGAIN,SOCKET_EWOULDBLOCK))){
+						$cancellation = $timeout > 0 ? new TimeoutCancellation($timeout) : null;
+						$cancellation?->throwIfCancelled();
+						$suspension = Loop::getSuspension();
+						$id = Loop::onReadable($this->getResource(),function(string $id) use($suspension,&$buffer,$length,$flags) : void {
+							Loop::cancel($id);
+							$result = @socket_recv($this->socket,$buffer,is_null($length) ? (1 << 16) : $length,is_null($length) ? 0 : $flags);
+							$suspension->resume($result);
+						});
+						$cancel = $cancellation?->subscribe($this->pending[$id] = static function() use($suspension,$id) : void {
+							Loop::cancel($id);
+							$suspension->resume(false);
+						});
+						try {
+							$result = $suspension->suspend();
+						} finally {
+							unset($this->pending[$id]);
+							$cancellation?->unsubscribe($cancel);
+						}
 					}
-				}
-				return $result ? $buffer : false;
+					return $result ? $buffer : false;
+			}
+		} else {
+			return false;
 		}
 	}
 	public function isClosed() : bool {
@@ -161,9 +182,9 @@ class StreamSocket {
 			case 'Revolt':
 				try {
 					array_map(call_user_func(...),$this->pending);
-					socket_close($this->socket);
+					@socket_close($this->socket);
 					if(is_resource($this->resource)){
-						fclose($this->resource);
+						@fclose($this->resource);
 					}
 					$this->resource = null;
 					$this->pending = [];
@@ -178,7 +199,7 @@ class StreamSocket {
 			case 'Swoole':
 				return $this->socket->getpeername();
 			case 'Revolt':
-				if(socket_getpeername($this->socket,$address,$port)){
+				if(@socket_getpeername($this->socket,$address,$port)){
 					return compact('address','port');
 				} else {
 					return false;
@@ -190,7 +211,7 @@ class StreamSocket {
 			case 'Swoole':
 				return $this->socket->getsockname();
 			case 'Revolt':
-				if(socket_getsockname($this->socket,$address,$port)){
+				if(@socket_getsockname($this->socket,$address,$port)){
 					return compact('address','port');
 				} else {
 					return false;
@@ -200,13 +221,13 @@ class StreamSocket {
 	public function setOption(int $option,mixed $value,int $level = SOL_SOCKET) : bool {
 		return match(Loop::name()){
 			'Swoole' => $this->socket->setOption($level,$option,$value),
-			'Revolt' => socket_set_option($this->socket,$level,$option,$value)
+			'Revolt' => @socket_set_option($this->socket,$level,$option,$value)
 		};
 	}
 	public function getOption(int $option,int $level = SOL_SOCKET) : mixed {
 		return match(Loop::name()){
 			'Swoole' => $this->socket->getOption($level,$option),
-			'Revolt' => socket_get_option($this->socket,$level,$option)
+			'Revolt' => @socket_get_option($this->socket,$level,$option)
 		};
 	}
 	public function setupTls(TlsContext $context) : bool {
@@ -216,17 +237,17 @@ class StreamSocket {
 				return $this->socket->sslHandshake();
 			case 'Revolt':
 				$stream = $this->getResource();
-				stream_context_set_options($stream,$context->toStreamContext());
+				@stream_context_set_options($stream,$context->toStreamContext());
 				do {
-					$result = stream_socket_enable_crypto($stream,true);
+					$result = @stream_socket_enable_crypto($stream,true);
 				} while(is_int($result));
 				return $result;
 		}
 	}
 	public function getResource() : mixed {
 		if(is_resource($this->resource) === false){
-			if($this->resource = socket_export_stream($this->socket)){
-				stream_set_blocking($this->resource,false);
+			if($this->resource = @socket_export_stream($this->socket)){
+				@stream_set_blocking($this->resource,false);
 			} else {
 				throw new \RuntimeException('socket_export_stream() failed');
 			}
